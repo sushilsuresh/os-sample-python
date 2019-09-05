db46dc95e8ba8e7ddfd86db4726f109925a93bac
Sushil Suresh <sushil.suresh@gmail.com>
2019-09-05 10:22:40 -0400

    Customise Hello World message

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import os
+from datetime import datetime
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    node = os.uname().nodename
+    dateTimeObj = datetime.now()
+    tstamp = dateTimeObj.strftime("%Y-%m-%d %H:%M:%S")
+    return "Hello World! - from - " + tstamp + ' - ' + node + '\n'
 
 if __name__ == "__main__":
     application.run()
